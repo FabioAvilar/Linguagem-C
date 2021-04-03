4fba0f98dbaa12294c96bb6a7e4abebee9087515
DeboraDawidiuk <81227272+DeboraDawidiuk@users.noreply.github.com>
2021-04-03 11:57:22 +0200

    C

---
+++ Hello World.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main () {
+    int A, B;
+
+    printf("\n Digite o valor 1: \t");
+    scanf("%d", &A);
+
+    printf("\n Digite o valor 2: \t");
+    scanf("%d", B);
+
+return 0;
+}
