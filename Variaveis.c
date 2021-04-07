066ba70a631f79bfd170d268120155145eea3cfc
DeboraDawidiuk <49881203+FabioAvilar@users.noreply.github.com>
2021-04-07 17:09:24 +0200

    #3 Variaveis
    Hoje aprendi alguns tipos de variaveis em C

---
+++ Variaveis.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[])
+{
+	
+	unsigned int numero1 = 15;
+	float numero2 = 22.5;
+	char caractere= 49;
+	
+	printf("%d\n", numero1);
+	printf("%.2f\n", numero2);
+	printf("%c\n", caractere);
+	system("PAUSE");
+	return 0;
+}
