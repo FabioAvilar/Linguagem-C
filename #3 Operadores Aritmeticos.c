4bb5a991b96a834e8c92c3fccce37182055a9e35
DeboraDawidiuk <49881203+FabioAvilar@users.noreply.github.com>
2021-04-08 15:40:24 +0200

    #3 Operadors Aritmeticos

---
+++ #3 Operadores Aritmeticos.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int  main(int argc, char *argv[])
+{
+	printf("%d\n", 5+2);//7
+	printf("%d\n", 9-4);//5
+	printf("%d\n", 2*3);//6
+	printf("%d\n", 10/5);//2
+	printf("%d\n", 9%4);//1
+		
+	system("PAUSE");
+	return 0;
+}
