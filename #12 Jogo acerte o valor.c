31916e57d4eec0838db4cf23a3d24c73af24a60e
DeboraDawidiuk <49881203+FabioAvilar@users.noreply.github.com>
2021-04-23 09:37:06 +0200

    #12 jogo acerte o valor

---
+++ #12 Jogo acerte o valor.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char*argv[])
+{
+	
+	int i;
+	
+	for(i=0; i!=12; )
+	{
+		printf("Digite o numero correto ");
+		scanf("%d", &i);
+	}
+	
+	system("PAUSE");
+	return 0;
+}
