c995ea169b5c40c95dab04f286cea33043a7630b
DeboraDawidiuk <49881203+FabioAvilar@users.noreply.github.com>
2021-06-04 06:31:36 +0200

    @1 Hello World

---
+++ @1 STDIO e STDLIB Hello World.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+//funçao principal do programa
+int main(int argc,char * argv[])
+{
+
+    printf("Hello World\n");
+
+    system("PAUSE");
+    return 0;
+}
