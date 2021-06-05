ff91c2c787064914d4faa7fa6530b9a7b70ee2ca
DeboraDawidiuk <49881203+FabioAvilar@users.noreply.github.com>
2021-06-05 14:30:07 +0200

    >

---
+++ @2 ImprimirConcatenar.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h
+
+
+
+int main(){
+//Definindo uma variavel
+    int a = 5;
+//Imprimindo a variavel  "a''
+    printf("%d", a);
+//Imprimindo
+    printf("\nUhuuuu");
+//Pausando
+    system("PAUSE");
+
+
+
+}
