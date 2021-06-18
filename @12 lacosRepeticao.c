ed2c63120cd8bcc263a1f8860f23d3fc48ea9fcb
DeboraDawidiuk <49881203+FabioAvilar@users.noreply.github.com>
2021-06-18 08:19:09 +0200

    .

---
+++ @12 lacosRepeticao.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+
+//funçao principal
+void main(){
+	
+	//definindo variavel
+	int a = 1;
+	
+	//contando ate 10
+	while(a<=10){
+		//imprimindo 'a' na tela
+		printf("\n%d",a);
+		
+		//incremento
+		a =a+1;
+	}
+	//pausa o sistema
+	system("pause");
+}
+
+
+
+
+
+
+
+
+
+
+
+
